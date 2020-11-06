4fd51f3d6676a7f477c159edf1bdb385b1401b46
soraping <soraping@163.com>
2020-11-06 17:54:44 +0800

    tree

--- structure/tree/binarySearchTree.ts
+++ structure/tree/binarySearchTree.ts
@@ -5,9 +5,9 @@ interface ITree<T> {
     // 中序遍历
     inOrderTraverse(cb: Function): void;
     // 先序遍历
-    preOrderTraverse(): void;
+    preOrderTraverse(cb: Function): void;
     // 后序遍历
-    postOrderTraverse(): void;
+    postOrderTraverse(cb: Function): void;
 }
 
 enum Compare {
@@ -47,30 +47,8 @@ class BinarySearchTree<T> implements IBinarySearchTree<T> {
         // 节点数递增
         this.count++
     }
-    // search(key: T){}
-    // remove(){}
 
-    /**
-     * 中序遍历
-     * 从最小到最大的顺序访问所有节点
-     * 对树进行排序操作
-     * @param cb 
-     */
-    inOrderTraverse(cb: Function){
-        this.inOrderTraverseNode(this.root, cb)
-    }
-
-    private inOrderTraverseNode(node: TreeNode<T>, cb: Function){
-        if(node != null){
-            // 先出小于的
-            this.inOrderTraverseNode(node.left, cb)
-            cb(node.key)
-            // 再出大于的
-            this.inOrderTraverseNode(node.rigth, cb)
-        }
-    }
-    
-    /**
+        /**
      * 
      * @param node 当前节点
      * @param key 
@@ -97,18 +75,84 @@ class BinarySearchTree<T> implements IBinarySearchTree<T> {
         if(a === b) return 0
         return a < b ? Compare.LESS_THEN : Compare.BIG_THEN
     }
+
+    // search(key: T){}
+    // remove(){}
+
+    /**
+     * 中序遍历
+     * 从最小到最大的顺序访问所有节点
+     * 对树进行排序操作
+     * @param cb 
+     */
+    inOrderTraverse(cb: Function){
+        this.inOrderTraverseNode(this.root, cb)
+    }
+
+    private inOrderTraverseNode(node: TreeNode<T>, cb: Function){
+        if(node != null){
+            // 先出小于的
+            this.inOrderTraverseNode(node.left, cb)
+            // console.log('node =>', node)
+            cb(node.key)
+            // 再出大于的
+            this.inOrderTraverseNode(node.rigth, cb)
+        }
+    }
+
+    /**
+     * 先序遍历
+     * 以优先于后代节点的顺序访问每个节点
+     * @param cb 
+     */
+    preOrderTraverse(cb: Function){
+        this.preOrderTraverseNode(this.root, cb)
+    }
+
+    private preOrderTraverseNode(node: TreeNode<T>, cb: Function){
+        if(node != null){
+            cb(node.key)
+            this.preOrderTraverseNode(node.left, cb)
+            this.preOrderTraverseNode(node.rigth, cb)
+        }
+    }
+
+    /**
+     * 后续遍历
+     * 先遍历节点的后代节点，再访问节点本身
+     * 应用场景：计算一个目录极其子目录所有文件所占用空间的大小
+     * @param cb 
+     */
+    postOrderTraverse(cb: Function){
+        this.postOrderTraverseNode(this.root, cb)
+    }
+
+    private postOrderTraverseNode(node: TreeNode<T>, cb: Function){
+        if(node != null){
+            this.postOrderTraverseNode(node.left, cb)
+            this.postOrderTraverseNode(node.rigth, cb)
+            cb(node.key)
+        }
+    }
+    
 }
 
 let tree = new BinarySearchTree<number>()
 
-tree.insert(10)
-tree.insert(1)
-tree.insert(12)
-tree.insert(3)
-tree.insert(22)
-tree.insert(7)
-tree.insert(13)
-tree.insert(9)
-tree.insert(33)
+tree.insert(11);
+tree.insert(7); 
+tree.insert(15); 
+tree.insert(5); 
+tree.insert(3); 
+tree.insert(9); 
+tree.insert(8); 
+tree.insert(10); 
+tree.insert(13); 
+tree.insert(12); 
+tree.insert(14); 
+tree.insert(20); 
+tree.insert(18); 
+tree.insert(25);
+tree.insert(6);
 
-console.log(tree.inOrderTraverse(console.log))
+tree.postOrderTraverse(console.log)
